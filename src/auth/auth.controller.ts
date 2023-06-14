be44feb8cebdf6a01ea7fb5e5a759d51bcf6993f
Javier <javierruizmora50@gmail.com>
2023-06-14 18:58:24 +0200

    Tercer commit, 346

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto, LoginDto, RegisterUserDto, UpdateAuthDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.authService.create(createUserDto);
   }
 
+  @Post('/login')
+  login( @Body() loginDto: LoginDto ) {
+    return this.authService.login( loginDto );
+  }
+
+  @Post('/register')
+  register( @Body() registerDto: RegisterUserDto ) {
+    return this.authService.register( registerDto );
+  }
+
+
+  @UseGuards( AuthGuard )
   @Get()
   findAll() {
     return this.authService.findAll();
